a233661c3675dd7296bfbf34f39d682f9e9eaf5e
Juho Pennanen <juho.pennanen@ecomond.com>
2023-12-08 19:17:48 +0200

    Added JSOUP library to classpath. Compiler updated.

--- compiler.bat
+++ compiler.bat
@@ -1,6 +1,8 @@
 @echo off
+cls
 cd src
-javac -d ..\bin myPackage\*.java
+javac -d ..\bin -cp ..\lib\jsoup-1.17.1.jar myPackage\*.java
+
 cd..
 cd bin
 jar cfm ..\Gatherer.jar ..\res\MANIFEST.MF myPackage\*.class
